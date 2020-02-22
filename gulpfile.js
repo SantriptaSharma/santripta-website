d2577e42eafb0e1e75d9c6e623c822862deb3e3f
Santripta Sharma <santripta@gmail.com>
2020-02-22 18:00:59 +0530

    Set up gulp

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
+
+gulp.task('reload', function(done){
+    browserSync.reload();
+    done();
+});
+
+gulp.task('serve', function(done){
+    browserSync.init({
+        server: "src/",
+        notify: false,
+        ghostMode: false,
+    })
+
+    gulp.watch("src/sass/*.scss", gulp.series('sass'));
+    gulp.watch("src/*.html", gulp.series('reload'));
+    done();
+});
+
+gulp.task('sass', function(){
+    return gulp.src("./src/sass/*.scss").pipe(sass()).pipe(gulp.dest("./src/")).pipe(browserSync.stream());
+});
+
+gulp.task('default', gulp.series('serve'));
